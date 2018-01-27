70b413046a9086e54099ce0cf81ff983272c2e9b
Kai Bakker <kaibakker@gmail.com>
2018-01-27 17:49:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import Form from './Form.js';
+import SearchForm from './SearchForm.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Form Validation Demo</h2>
+          <h2>🔍 Search, 🌏 Explorer and 💸 Pay any crypto address</h2>
+          <br />
+          <SearchForm />
+          <br />
+          <h4>
+            Some example addresses to try
+          </h4>
+          <code>1HB5XMLmzFVj8ALj6mfBsbifRoD4miY36v</code><br />
+          <code>LPpVeFSKvH593CChqP9qpV5toEXntekjiF</code><br />
+          <code>0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae</code><br />
+
         </div>
-        <Form />
-        <h4>
-          From <a style={{textDecoration: 'underline'}} href="https://learnetto.com/users/hrishio/courses/the-complete-react-on-rails-5-course">The Complete React on Rails Course</a>
-        </h4>
+
+
       </div>
     );
   }
